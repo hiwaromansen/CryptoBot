be92649e04405dcade30859117e6886c172a375f
hiwaromansen <hiwaromansen@gmail.com>
2026-07-03 12:31:12 +0330

    add new function fetch_ticker to collect_btcc_ticks.py

--- collect_data/collect_btcc_ticks.py
+++ collect_data/collect_btcc_ticks.py
@@ -34,3 +34,20 @@ logger = logging.getLogger(__name__)
 client = MongoClient()
 collection = client[DATABASE][COLLECTION]
 
+def fetch_ticker() -> dict[str, Any]:
+    """
+    Download ticker information from BTCC.
+    """
+
+    response = requests.get(TICKER_URL, timeout=REQUEST_TIMEOUT)
+    response.raise_for_status()
+
+    ticker = response.json()["ticker"]
+
+    ticker["_id"] = int(ticker.pop("date"))
+
+    for key, value in ticker.items():
+        ticker[key] = float(value)
+
+    return ticker
+
