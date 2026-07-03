00b4dcb7d8733f8ba5cfd76fcb4c89d0d32eecb2
hiwaromansen <hiwaromansen@gmail.com>
2026-07-03 12:23:47 +0330

    add new script crawl_btcc_trades_hostory.py

---
+++ collect_data/crawl_btcc_trades_history.py
@@ -0,0 +1,38 @@
+"""
+Download historical BTCC trades and store them in MongoDB.
+
+Usage:
+    python crawl_btcc_trades_history.py
+    python crawl_btcc_trades_history.py --continuous
+"""
+
+from __future__ import annotations
+
+import argparse
+import logging
+import time
+from datetime import datetime
+from typing import Any
+
+import requests
+from pymongo import MongoClient, UpdateOne
+
+API_URL = "http://data.btcchina.com"
+TRADES_URL = API_URL + "/data/historydata?since={since}&limit=5000&sincetype=time"
+
+DATABASE = "cryptobot"
+COLLECTION = "btcc_btccny_trades"
+
+REQUEST_TIMEOUT = 5
+REQUEST_DELAY = 1.0
+START_TIMESTAMP = 1476093600
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+)
+
+logger = logging.getLogger(__name__)
+
+client = MongoClient()
+collection = client[DATABASE][COLLECTION]
